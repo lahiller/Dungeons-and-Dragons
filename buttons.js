bec22c2bafcebcf62851f50bd4bbeed5e72761c2
lahiller <lahiller@icloud.com>
2017-06-09 19:34:42 -0400

    Create buttons.js

--- buttons.js
+++ buttons.js
@@ -7,23 +7,23 @@
 
 <p id="d20appear">Your d20 Roll will appear here.</p>
 
-<button onclick="document.getElementById('d20appear').innerHTML = getRndInteger(1, 20)">Roll a d20</button>
+<button onclick="document.getElementById('d20appear').innerHTML = getRndInteger(1, 21)">Roll a d20</button>
 
 <p id="d10appear">Your d10 Roll will appear here.</p>
 
-<button onclick="document.getElementById('d10appear').innerHTML = getRndInteger(1, 10)">Roll a d10</button>
+<button onclick="document.getElementById('d10appear').innerHTML = getRndInteger(1, 11)">Roll a d10</button>
 
 <p id="d8appear">Your d8 Roll will appear here.</p>
 
-<button onclick="document.getElementById('d8appear').innerHTML = getRndInteger(1, 8)">Roll a d8</button>
+<button onclick="document.getElementById('d8appear').innerHTML = getRndInteger(1, 9)">Roll a d8</button>
 
 <p id="d6appear">Your d6 Roll will appear here.</p>
 
-<button onclick="document.getElementById('d6appear').innerHTML = getRndInteger(1, 6)">Roll a d6</button>
+<button onclick="document.getElementById('d6appear').innerHTML = getRndInteger(1, 7)">Roll a d6</button>
 
 <p id="d4appear">Your d4 Roll will appear here.</p>
 
-<button type="button" onclick="document.getElementById('d4appear').innerHTML = getRndInteger(1, 4)">Roll a d4</button>
+<button onclick="document.getElementById('d4appear').innerHTML = getRndInteger(1, 5)">Roll a d4</button>
 
 <script>
 function getRndInteger(min, max) {
